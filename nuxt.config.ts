1b519cd11401bd40aefe8f5e31b23eb894baa73c
Joao DALLA ROSA <joao.dallarosa.ext@louisvuitton.com>
2022-06-25 01:36:46 +0200

    Update deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'browser'
-  },
   build: {
     postcss: {
       postcssOptions: {
