9605526dfdbf3b19df18316190fa94af1330a792
Joao DALLA ROSA <joao.dallarosa.ext@louisvuitton.com>
2022-06-25 01:39:34 +0200

    Rollback

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto bg-gray-300 p-8">
     <h1 class="font-bold text-gray-600 text-lg">Stuff</h1>
-    <!-- <ul>
+    <ul>
       <li v-for="piece in list" :key="piece.url">
         <NuxtLink :to="`/pieces/${piece.url}`">
           <button>
@@ -9,21 +9,21 @@
           </button>
         </NuxtLink>
       </li>
-    </ul> -->
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-// import { onMounted } from "vue";
-// const canvas = ref(null);
-// let list = []
+import { onMounted } from "vue";
+const canvas = ref(null);
+let list = []
 
-// if (process.client) {
-//   const {pieces} = await import("~~/src/pieces");
-//   console.log('PIECES', pieces)
-//   list = pieces
-//   onMounted(() => {
-//     console.log(window.innerWidth, window.innerHeight);
-//   });
-// }
+if (process.client) {
+  const {pieces} = await import("~~/src/pieces");
+  console.log('PIECES', pieces)
+  list = pieces
+  onMounted(() => {
+    console.log(window.innerWidth, window.innerHeight);
+  });
+}
 </script>
