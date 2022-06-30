071fef3ae94572a620f6059c1d5ca1dd47537c26
Joao DALLA ROSA <joao.dallarosa.ext@louisvuitton.com>
2022-06-30 11:09:35 +0200

    Testing Pre Render Routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,15 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   target: "static",
-  // ssr: false,
+  ssr: false,
   router: {
-    base: '/p5-showcase/',
+    base: "/p5-showcase/",
+  },
+  generate: {
+    subFolders: false,
   },
   app: {
-    baseURL: '/p5-showcase/'
+    baseURL: "/p5-showcase/",
   },
   build: {
     postcss: {
@@ -26,4 +29,14 @@ export default defineNuxtConfig({
     },
   },
   css: ["~/assets/css/tailwind.css"],
+  nitro: {
+    prerender: {
+      routes: [
+        "/p5-showcase/three",
+        "/p5-showcase/fragment",
+        "/p5-showcase/pieces/not-art",
+        "/p5-showcase/pieces/boyhood",
+      ],
+    },
+  },
 });
